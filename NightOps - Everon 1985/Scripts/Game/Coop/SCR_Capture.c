a0b279329fddcac749cae39c09374c00c7c8c1a7
Moritz <66870570+Zealous666@users.noreply.github.com>
2022-05-24 18:54:48 +0200

    Add files via upload
    - reworked weater & daytime manager
    - added custom loadouts
    - added template (m_00)

--- NightOps - Everon 1985/Scripts/Game/Coop/SCR_Capture.c
+++ NightOps - Everon 1985/Scripts/Game/Coop/SCR_Capture.c
@@ -1,10 +1,17 @@
 class SCR_CaptureVehicle : ScriptedUserAction
 {
+	
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 		{
-		SCR_PopUpNotification.GetInstance().PopupMsg("RADIO TRUCK CAPTURED");
 		
-		 SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode()); // Get the game mode for the end script
+		//SCR_EditorTask task = SCR_EditorTask.Cast(pOwnerEntity.GetWorld().FindEntityByName("obj_primaryTsk_1"));
+		//GetTaskManager().FinishTask(task);
+		
+		
+	
+		SCR_PopUpNotification.GetInstance().PopupMsg("COM TRUCK CAPTURED", duration: 5);
+		
+		SCR_BaseGameMode gameMode = SCR_BaseGameMode.Cast(GetGame().GetGameMode()); // Get the game mode for the end script
         Faction faction = GetGame().GetFactionManager().GetFactionByKey("US"); // Get the winning faction Key
         int usIndex = GetGame().GetFactionManager().GetFactionIndex(faction); // Get the winning faction key's index
         gameMode.EndGameMode(SCR_GameModeEndData.CreateSimple(SCR_GameModeEndData.ENDREASON_EDITOR_FACTION_VICTORY, -1, usIndex)); // End the mission!
